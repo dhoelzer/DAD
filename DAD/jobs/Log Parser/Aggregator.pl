02febf8f54c15d70731b4b8e913258e100053cfa
David Hoelzer <dhoelzer@enclaveforensics.com>
2007-03-25 14:06:05 +0000

    Split Stat generation out of the Aggregator in preparation for the Java scheduler.

--- DAD/jobs/Log Parser/Aggregator.pl
+++ DAD/jobs/Log Parser/Aggregator.pl
@@ -26,7 +26,6 @@ use Thread::Queue;
 use DBI;
 use POSIX;
 use Win32::EventLog;
-use GD::Graph::lines;
 
 ################################################################
 #
@@ -96,13 +95,6 @@ while(1)						#Always running, never getting anywhere
 	$loop++;					# Number of times through this run
 #	$Time_Remaining = $Total_Run_Time - (mktime(localtime())- $System_Started);
 	$Time_Remaining = "Continuously running.";
-	if((($loop % 120) == 0))		# Update the stat graphs every 10 minutes
-	{
-		open(STATS, ">>$OUTPUT_LOCATION/stats.html") or print "Couldn't open stats file.\n";
-		print STATS "Building statistics\n";
-		close(STATS);
-		&_build_stats();		# This locks the main thread during stat creation
-	}
 	{
 		#Recreate the quick stats every time through.
 		open(STATS, ">$OUTPUT_LOCATION/stats.html") or print "Couldn't open stats file.\n";
@@ -143,24 +135,6 @@ End
 		}
 		close(STATS);
 	}
-# Disabled for testing	if($Time_Remaining < 1)				#If we've run out of time and there's no groomer, signal the end
-#	{
-#		if((!$Groomer_Running) && (!$Pending_Running)) { $Time_To_Die = 1; }
-#		if(($SQL_Queue->pending()==0)) #Enough time for the SQL threads to complete if sleeping
-#		{
-#			my $safe_to_die = 1;
-#			foreach $status ((sort keys(%Status)))
-#			{
-#				if ($Status{$status} ne "Dead.") { $safe_to_die = 0; }
-#			}
-#			if($safe_to_die)
-#			{
-#				print "All log threads dead, SQL queue empty.  Force exiting.\n";
-#				exit;
-#			}
-#			else { print "There are still locked threads pending.  Delaying exit.\n"; }
-#		}
-#	}
 							#See which queues are waiting and queue them as appropriate
 	@Systems = &_get_systems_to_process;
 	foreach $system (@Systems)
@@ -257,10 +231,6 @@ sub _event_thread
 			"'$values[10]', '$values[11]', '$values[12]', '$values[13]', '$values[14]', '$values[15]', '$values[16]', ".
 			"'$values[17]', '$values[18]', '$values[19]', '$values[20]', '$values[21]', '$values[22]', '$values[23]',".
 			"'$values[24]', '$values[25]', '$idxID_Code', '$idxID_Kerb', '$idxID_NTLM')");
-#			/* idxID_Code = EventID + Field_2
-#			   idxID_Kerb = EventID + Field_4
-#			   idxID_NTLM = EventID + Field_3
-#			*/
 
 	}
 
@@ -804,106 +774,6 @@ sub SQL_Batch_Insert
 	$dbh->do($SQL);
 }
 
-sub _build_stats
-{
-	my $system;
-	my %ALog_Size, %AInserted, %Log_Size, %Inserted;
-	my @Systems;					#Systems to process
-	@Systems = &_get_stats_to_process();
-
-	foreach $system(@Systems)
-	{
-		my $size, @times, $i;
-		my $log_change, $inserted, $insert_ratio;
-		my @Times, @Logged, @Inserted, $this_time;
-	
-		$insert_ratio = 0;
-		&_get_system_stat_data($system,\%Log_Size,\%Inserted, \%ALog_Size, \%AInserted, "Security", $Stat_Time_Period);
-		@times = reverse(sort(keys(%Log_Size)));
-		$size=@times;
-	
-		foreach $i (0..($size))
-		{
-			$log_change = $Log_Size{$times[$i]};
-			$inserted = $Inserted{$times[$i]};
-			$this_time = &_get_time_string($times[$i]);
-			unshift(@Times, $this_time);
-			unshift(@Logged, $log_change);
-			unshift(@Inserted, $inserted);
-			if($inserted < 0) {print "$system: $this_time Logged - $log_change  Inserted - $inserted  Difference - $insert_ratio\n";}
-		}
-		my $points = @Times;
-		if($points)
-		{
-			my $graph = GD::Graph::lines->new(400, 100);
-			$graph->set( 
-				title				=> "$system Event/Insert Rate",
-				x_label_position	=> 0.5,
-				line_width			=> 1,
-				x_label_skip		=> int($points/10),
-				x_labels_vertical	=> 1
-			) or die $graph->error;
-			my @Data=([@Times],[@Logged],[@Inserted]);#,[@Insert_Ratio]);
-			$graph->set_title_font('/fonts/arial.ttf', 24);
-			my $gd = $graph->plot(\@Data) or die $graph->error;
-			open(IMG, '>'.$OUTPUT_LOCATION."/$system.gif") or die $!;
-			binmode IMG;
-			print IMG $gd->gif;
-			close IMG;
-		}
-		@Times=();
-		@Logged=();
-		@Inserted=();
-		@Insert_Ratio=();
-		%Log_Size = ();
-		%Inserted = ();
-	}
-	print "Generating aggregate.\n";
-	@Times=();
-	@Logged=();
-	@Inserted=();
-	@Insert_Ratio=();
-	@times=();
-	@Data=();
-	%ALog_Size = ();
-	%AInserted = ();
-	foreach $system (@Systems)
-	{
-		&_get_aggregate_system_stat_data($system,\%Log_Size,\%Inserted, \%ALog_Size, \%AInserted, "Security", time());
-		#Previous line gathers all aggregate data for all time
-	}
-	@times = reverse(sort(keys(%ALog_Size)));
-	$size=@times;
-	foreach $i (0..($size))
-	{
-		$log_change = $ALog_Size{$times[$i]};
-		$inserted = $AInserted{$times[$i]};
-		$this_time = &_get_time_string($times[$i],1);
-		unshift(@Times, $this_time);
-		unshift(@Logged, $log_change);
-		unshift(@Inserted, $inserted);
-		if($inserted < 0) {print "$system: $this_time Logged - $log_change  Inserted - $inserted  Difference - $insert_ratio\n";}
-		#print "$system: $this_time Logged - $log_change  Inserted - $inserted  Difference - $insert_ratio\n";
-	}
-	
-	#Print Aggregate
-	my $points = @Times;
-	my $graph = GD::Graph::lines->new(400,200);
-	$graph->set( 
-		title				=> "$system Event/Insert Rate",
-		x_label_position	=> 0.5,
-		line_width			=> 1,
-		x_label_skip		=> int($points/10),
-		x_labels_vertical	=> 1
-	) or die $graph->error;
-	my @Data=([@Times],[@Logged],[@Inserted]);#,[@Insert_Ratio]);
-	$graph->set_title_font('/fonts/arial.ttf', 24);
-	my $gd = $graph->plot(\@Data) or die $graph->error;
-	open(IMG, '>'.$OUTPUT_LOCATION.'/Aggregate.gif') or die $!;
-	binmode IMG;
-	print IMG $gd->gif;
-	close IMG;
-	return;
 
 sub _get_time_string
 {
@@ -921,105 +791,6 @@ sub _get_time_string
 		$theTime = ($hour<10 ? "0$hour" : "$hour").":".($minute<10? "0$minute" : "$minute");
 	}
 	return $theTime; 
-}
-##########################
-# Grabs the raw data for each system.
-sub _get_system_stat_data
-{
-	my	$results_ref,				# Used to hold query responses
-		$row,						#Row array reference
-		@this_row;					#Current row
-	my $system, $Log_Size, $Inserted, $ALog, $AInserted, $Service;
-	my $Time_Period;
-	($system,$Log_Size,$Inserted, $ALog, $AInserted, $Service, $Time_Period)=@_ or die("Incorrect arguments to _get_system_stat_data.\n");
-	
-	$dsn = "DBI:mysql:host=$MYSQL_SERVER;database=DAD";
-	$dbh = DBI->connect ($dsn, "$MYSQL_USER", "$MYSQL_PASSWORD")
-		or die ("Could not connect to DB server to import the list of servers to poll.\n");
-
-	$Time_Period = time()-$Time_Period;
-	my $SQL = "SELECT Total_In_Log,Number_Inserted,Stat_Time,Service_Name FROM dad_sys_event_stats WHERE Service_Name='$Service' AND System_Name='$system' AND Stat_Time>$Time_Period ORDER BY Stat_Time";
-	$results_ref = &SQL_Query($SQL);
-	my $last_logged = -1;
-	while($row = shift(@$results_ref) ) 
-	{
-		@this_row = @$row;
-		my $this_time = $this_row[2];
-		$this_time = int($this_time/600) * 600;
-		if($this_row[0] > 0)
-		{
-			my $log_change = 0;
-			if($last_logged != -1)
-				{$log_change = $this_row[0] - $last_logged};
-			$Log_Size->{$this_time} += $log_change;
-			$last_logged = $this_row[0];
-			$Inserted->{$this_time} += $this_row[1];
-			$ALog->{$this_time} += $log_change;
-			$AInserted->{$this_time} += $this_row[1];
-		}
-	}
-	return;
-}
-
-##########################
-# Grabs the raw data for each system.
-sub _get_aggregate_system_stat_data
-{
-	my	$results_ref,				# Used to hold query responses
-		$row,						#Row array reference
-		@this_row;					#Current row
-	my $system, $Log_Size, $Inserted, $ALog, $AInserted, $Service;
-	my $Time_Period;
-	($system,$Log_Size,$Inserted, $ALog, $AInserted, $Service, $Time_Period)=@_ or die("Incorrect arguments to _get_system_stat_data.\n");
-	
-	$dsn = "DBI:mysql:host=$MYSQL_SERVER;database=DAD";
-	$dbh = DBI->connect ($dsn, "$MYSQL_USER", "$MYSQL_PASSWORD")
-		or die ("Could not connect to DB server to import the list of servers to poll.\n");
-
-	$Time_Period = time()-$Time_Period;
-	my $SQL = "SELECT Total_In_Log,Number_Inserted,Stat_Time,Service_Name FROM dad_sys_event_stats WHERE Service_Name='$Service' AND System_Name='$system' AND Stat_Time>$Time_Period ORDER BY Stat_Time";
-	$results_ref = &SQL_Query($SQL);
-	my $last_logged = -1;
-	while($row = shift(@$results_ref) ) 
-	{
-		@this_row = @$row;
-		my $this_time = $this_row[2];
-		$this_time = int($this_time/86400) * 86400;
-		if($this_row[0] > 0)
-		{
-			my $log_change = 0;
-			if($last_logged != -1)
-				{$log_change = $this_row[0] - $last_logged};
-			$last_logged = $this_row[0];
-			$ALog->{$this_time} += $log_change;
-			$AInserted->{$this_time} += $this_row[1];
-		}
-	}
-	return;
-}
-
-
-##########################
-# Grabs the names of systems ripe for processing
-sub _get_stats_to_process
-{
-	my	$results_ref,				# Used to hold query responses
-		$row,						#Row array reference
-		@this_row;					#Current row
-	my @Systems;
-	
-
-	# Fetch the names of the systems to poll.  To add a system simply add its name to the dad_sys_event_import table. 
-	# There is no need to restart this process to pick up the new system names or remove old names.
-	$results_ref = &SQL_Query("SELECT DISTINCT System_Name FROM dad_sys_event_stats");
-	# Populate the @Systems array
-	while($row = shift(@$results_ref) ) 
-	{
-		@this_row = @$row;
-		unshift(@Systems, $this_row[0]);
-	}
-	return(@Systems);
-}
 }
 
 sub _insert_pending_events
