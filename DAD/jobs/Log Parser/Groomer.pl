98b41fe0de415596f52f02be3c7cfdf42ab0d5bb
David Hoelzer <dhoelzer@enclaveforensics.com>
2007-08-08 12:44:47 +0000

    Reworked groomer to prune events into a temp table rather than into dad_sys_events.  This allows the aggregator to continue to function correctly and jobs to continue to fire correctly.
    After the groomer has completed, it rotates the tmp table into the dad_sys_events and then selects all of the new data into dad_sys_events.

--- DAD/jobs/Log Parser/Groomer.pl
+++ DAD/jobs/Log Parser/Groomer.pl
@@ -158,18 +158,69 @@ sub _groomer
 ") ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='Normalized Windows Events' ";
 
 		$dbh->do($SQL) or die("Could not create pruner table.");
+	$SQL = "CREATE TABLE dad_sys_events_tmp (".
+  "`dad_sys_events_id` int(10) unsigned NOT NULL auto_increment,".
+  "`SystemID` mediumint(8) unsigned NOT NULL default '0',".
+  "`ServiceID` mediumint(8) unsigned NOT NULL default '0',".
+  "`TimeWritten` int(10) unsigned NOT NULL default '0',".
+  "`TimeGenerated` int(10) unsigned NOT NULL default '0',".
+  "`Source` char(255) NOT NULL default '',".
+  "`Category` char(255) NOT NULL default '',".
+  "`SID` char(64) character set latin1 NOT NULL default '',".
+  "`Computer` char(255) NOT NULL default '',".
+  "`EventID` mediumint(8) unsigned NOT NULL default '0',".
+  "`EventType` tinyint(3) unsigned NOT NULL default '0',".
+  "`Field_0` varchar(760) default NULL,".
+  "`Field_1` varchar(760) default NULL,".
+  "`Field_2` varchar(760) default NULL,".
+  "`Field_3` varchar(760) default NULL,".
+  "`Field_4` varchar(760) default NULL,".
+  "`Field_5` varchar(760) default NULL,".
+  "`Field_6` varchar(760) default NULL,".
+  "`Field_7` varchar(760) default NULL,".
+  "`Field_8` varchar(760) default NULL,".
+  "`Field_9` varchar(760) default NULL,".
+  "`Field_10` varchar(760) default NULL,".
+  "`Field_11` varchar(760) default NULL,".
+  "`Field_12` varchar(760) default NULL,".
+  "`Field_13` varchar(760) default NULL,".
+  "`Field_14` varchar(760) default NULL,".
+  "`Field_15` varchar(760) default NULL,".
+  "`Field_16` varchar(760) default NULL,".
+  "`Field_17` varchar(760) default NULL,".
+  "`Field_18` varchar(760) default NULL,".
+  "`Field_19` varchar(760) default NULL,".
+  "`Field_20` varchar(760) default NULL,".
+  "`Field_21` varchar(760) default NULL,".
+  "`Field_22` varchar(760) default NULL,".
+  "`Field_23` varchar(760) default NULL,".
+  "`Field_24` varchar(760) default NULL,".
+  "`Field_25` varchar(760) default NULL,".
+  "`idxID_Code` char(64) default NULL,".
+  "`idxID_Kerb` char(64) default NULL,".
+  "`idxID_NTLM` char(64) default NULL,".
+  "PRIMARY KEY  (`dad_sys_events_id`),".
+  "KEY `idxEventID` (`EventID`),".
+  "KEY `idxSID` (`SID`),".
+  "KEY `idxTimestamp` (`TimeGenerated`),".
+  "KEY `idxIDbyCode` (`idxID_Code`(10)),".
+  "KEY `idxIDbyKerb` (`idxID_Kerb`(15)),".
+  "KEY `idxIDbyNTLM` (`idxID_NTLM`(10)),".
+  "KEY `idxNTLMCode` (`Field_3`(15))".
+") ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='Normalized Windows Events' ";
+		$dbh->do($SQL) or die("Could not create pruner table.");
 			
 		$SQL = "RENAME TABLE dad_sys_events TO dad_tmp, dad_sys_events_pruning TO dad_sys_events, dad_tmp TO dad_sys_events_pruning";
 		$dbh->do($SQL) or die("Could not rename tables for groomer.");
-		@Events_To_PruneX={};
-		foreach $event (@Events_To_PruneX)
+
+		foreach $event (@Events_To_Prune)
 		{
 			if($event == 0) { next; } # Don't try to process default rule
 			if($Output)
 			{
 				print "Pruning event ID $event\n";
 			}
-		$SQL = "INSERT INTO dad_sys_events (SystemID, ServiceID, TimeWritten, TimeGenerated, Source, ".
+		$SQL = "INSERT INTO dad_sys_events_tmp (SystemID, ServiceID, TimeWritten, TimeGenerated, Source, ".
 				"Category, SID, Computer, EventID, EventType, Field_0, Field_1, Field_2, Field_3, Field_4, Field_5, ".
 				"Field_6, Field_7, Field_8, Field_9, Field_10, Field_11, Field_12, Field_13, Field_14, Field_15, Field_16, ".
 				"Field_17, Field_18, Field_19, Field_20, Field_21, Field_22, Field_23, Field_24, Field_25, idxID_Code, ".
@@ -178,7 +229,7 @@ sub _groomer
 				"Field_6, Field_7, Field_8, Field_9, Field_10, Field_11, Field_12, Field_13, Field_14, Field_15, Field_16, ".
 				"Field_17, Field_18, Field_19, Field_20, Field_21, Field_22, Field_23, Field_24, Field_25, idxID_Code, ".
 				"idxID_Kerb, idxID_NTLM FROM dad_sys_events_pruning WHERE EventID='$event' AND TimeGenerated>".(time() - $Retention_Times{$event});
-			if($Output) { print "\t$SQL\n".time()." - ".$Retention_Times{$event}."\n"; }
+#			if($Output) { print "\t$SQL\n".time()." - ".$Retention_Times{$event}."\n"; }
 			my $query = $dbh->prepare($SQL);
 			$query->execute() or die("Error pruning!  Could not complete prune for $event.");
 			$query->finish();
@@ -187,7 +238,7 @@ sub _groomer
 		{
 			print "Pruning default rule\n";
 		}
-		$SQL = "INSERT INTO dad_sys_events (SystemID, ServiceID, TimeWritten, TimeGenerated, Source, ".
+		$SQL = "INSERT INTO dad_sys_events_tmp (SystemID, ServiceID, TimeWritten, TimeGenerated, Source, ".
 				"Category, SID, Computer, EventID, EventType, Field_0, Field_1, Field_2, Field_3, Field_4, Field_5, ".
 				"Field_6, Field_7, Field_8, Field_9, Field_10, Field_11, Field_12, Field_13, Field_14, Field_15, Field_16, ".
 				"Field_17, Field_18, Field_19, Field_20, Field_21, Field_22, Field_23, Field_24, Field_25, idxID_Code, ".
@@ -205,7 +256,6 @@ sub _groomer
 			$SQL .= " AND NOT EventID='$event'";
 		}
 		if($Output){print "Default rule: $SQL\n";}
-exit 1;
 		my $query = $dbh->prepare($SQL);
 		$query->execute() or die("Error pruning!  Could not complete default rule.");
 		$query->finish();
@@ -213,16 +263,39 @@ exit 1;
 		{
 			print "Pruning Complete.  Pruning took ".(time()-$start_time)." seconds.\n";
 		}
-		$SQL = "DROP TABLE dad_sys_events_pruning";
-		$dbh->do($SQL);
+	$SQL = "DROP TABLE dad_sys_events_pruning";
+	$dbh->do($SQL);
 
-	$results_ref = &SQL_Query("SELECT COUNT(*) FROM dad_sys_events");
+	$results_ref = &SQL_Query("SELECT COUNT(*) FROM dad_sys_events_tmp");
 	$row = shift(@$results_ref);
 	$ending_number = @$row[0];
 	if($Output)
 	{
 		print "There were $starting_number events, there are now $ending_number events.  Groomed ".($starting_number - $ending_number).".\n";
 	}
+	if($Output)
+	{
+		print "Reshuffling table and inserting new events.\n";
+	}
+	$SQL = "RENAME TABLE dad_sys_events TO dad_tmp, dad_sys_events_tmp TO dad_sys_events, dad_tmp TO dad_sys_events_pruning";
+	$dbh->do($SQL) or die("Could not rename tables for groomer.");
+	$SQL = "INSERT INTO dad_sys_events (SystemID, ServiceID, TimeWritten, TimeGenerated, Source, ".
+				"Category, SID, Computer, EventID, EventType, Field_0, Field_1, Field_2, Field_3, Field_4, Field_5, ".
+				"Field_6, Field_7, Field_8, Field_9, Field_10, Field_11, Field_12, Field_13, Field_14, Field_15, Field_16, ".
+				"Field_17, Field_18, Field_19, Field_20, Field_21, Field_22, Field_23, Field_24, Field_25, idxID_Code, ".
+				"idxID_Kerb, idxID_NTLM) SELECT SystemID, ServiceID, TimeWritten, TimeGenerated, Source, ".
+				"Category, SID, Computer, EventID, EventType, Field_0, Field_1, Field_2, Field_3, Field_4, Field_5, ".
+				"Field_6, Field_7, Field_8, Field_9, Field_10, Field_11, Field_12, Field_13, Field_14, Field_15, Field_16, ".
+				"Field_17, Field_18, Field_19, Field_20, Field_21, Field_22, Field_23, Field_24, Field_25, idxID_Code, ".
+				"idxID_Kerb, idxID_NTLM FROM dad_sys_events_pruning";
+	my $query = $dbh->prepare($SQL);
+	$query->execute() or die("Could not copy back new records!");
+	$query->finish();
+	if($Output){ print "Dropping tmp table.\n"; }
+	$SQL = "DROP TABLE dad_sys_events_pruning";
+	$dbh->do($SQL);
+
+
 
 }
 	
