b8610ec2eba485e8d01b590a21136ce15c995572
David Hoelzer <dhoelzer@enclaveforensics.com>
2009-03-17 16:12:38 +0000

    Started building Perl based scheduler to start replacing Java code in the project.

---
+++ DAD/jobs/Scheduler.pl
@@ -0,0 +1,14 @@
+#!c:/perl/bin/perl.exe
+
+# Scheduler.pl
+# DAD
+#
+# Created by David Hoelzer on 3/17/09.
+# Copyright 2009 Enclave Forensics, All rights reserved.
+
+# This Perl script completely replaces the Java scheduler to allow for easier portability.
+# This script requires Win32::Process.  A future version of the script will be created for
+# UNIX based systems using Proc::Simple, which is broken in Win32 implementations.
+
+
+
