1a36014d086c7427513a0605f116ccb860679a7d
David Hoelzer <dhoelzer@enclaveforensics.com>
2009-03-17 16:54:02 +0000

    Initial testing version of scheduler

--- DAD/jobs/Scheduler.pl
+++ DAD/jobs/Scheduler.pl
@@ -10,5 +10,93 @@
 # This script requires Win32::Process.  A future version of the script will be created for
 # UNIX based systems using Proc::Simple, which is broken in Win32 implementations.
 
+require "dbconfig.ph";
 
+&DB_Connect;
+@PendingJobs = &_get_pending_jobs;
+foreach(@PendingJobs)
+{
+	print "Pending: $Descriptions{$_} : $CommandLines{$_}\n";
+}
+
+sub _get_pending_jobs
+{
+	my	$results_ref,				# Used to hold query responses
+	$row,						#Row array reference
+	@this_row;					#Current row
+	my @TheseJobs;
+	
+	$results_ref = &SQL_Query("SELECT id_dad_adm_job, descrip, path, package_name, argument_1, next_start, is_running, persistent FROM dad_adm_job");
+	while($row = shift(@$results_ref) ) 
+	{
+		@this_row = @$row;
+		if(mktime(localtime()) > $this_row[5]) 
+		{
+			unshift(@TheseJobs, $this_row[0]);
+			$Descriptions{$this_row[0]} = $this_row[1];
+			$CommandLines{$this_row[0]} = "$this_row[3] $this_row[4]";
+			$Paths{$this_row[0]} = $this_row[2];
+		}
+	}
+	undef $results_ref;
+	undef $row;
+	return(@TheseJobs);
+}
+
+##################################################
+#
+# DB_Connect - Builds initial connection to database server
+#
+##################################################
+sub DB_Connect
+{
+	undef $dsn;
+	undef $dhb;
+	$dsn = "DBI:mysql:host=$MYSQL_SERVER;database=dad";
+	$dbh = DBI->connect ($dsn, "$MYSQL_USER", "$MYSQL_PASSWORD")
+	or die ("Could not connect to DB server\n");
+}
+
+##################################################
+#
+# SQL_Query - Does the legwork for all SQL queries including basic error checking
+# 	Takes a SQL string as an argument
+#
+##################################################
+sub SQL_Query
+{
+	my $SQL = $_[0];
+	
+	my $query = $dbh->prepare($SQL);
+	eval
+	{
+		$query -> execute() or die("$SQL\n");
+	};
+	if($@){
+		print "Caught error: $@\n";
+		&DB_Connect();
+		$query->execute();
+	};
+	my $ref_to_array_of_row_refs = $query->fetchall_arrayref() or die("$SQL\n"); 
+	$query->finish();
+	return $ref_to_array_of_row_refs;
+}
+
+##################################################
+#
+# SQL_Insert - Does the legwork for all SQL inserts including basic error checking
+# 	Takes a SQL string as an argument
+#
+##################################################
+sub SQL_Insert
+{
+	my $SQL = $_[0];
+	my $query = $dbh->prepare($SQL);
+	if($DEBUG){return; print"$SQL\n";return;}
+	$query -> execute();
+	my $in_id = $dbh->{ q{mysql_insertid}};
+	$query->finish();
+	undef $query;
+	return $in_id;
+}
 
