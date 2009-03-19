2a393991e7ec18fad4709a0de036a2c89ccc61ac
David Hoelzer <dhoelzer@enclaveforensics.com>
2009-03-19 14:09:16 +0000

    First fully functional version of perl syslog integrated into scheduler.

--- DAD/jobs/syslog.pl
+++ DAD/jobs/syslog.pl
@@ -1,9 +1,16 @@
 #!c:/perl/bin/perl.exe
 use IO::Socket;
+use Time::Local;
+use POSIX;
+
+my $buf;
+my $Filename, $RotateTime = 0;
+
+$LOGSTAGING="C:/dad/jobs/LogStaging";
+$LOGPROCESSING="C:/dad/jobs/LogsToProcess";
 
 $sock = IO::Socket::INET->new(LocalPort => '514', Proto => 'udp')||die("Socket: $@");
 
-my $buf;
 do{
 	$sock->recv($buf, 1540);
 	my ($port, $ipaddr) = sockaddr_in($sock->peername);
@@ -16,5 +23,22 @@ do{
 sub logit{
 	my $host = shift;
 	my $message = shift;
+	&RotateLog;
 	print "h:$host m:$message\n";
+}
+
+sub RotateLog
+{
+	if(mktime(localtime()) > $RotateTime)
+	{
+		if($RotateTime > 0)
+		{
+			close(LOG);
+			rename "$LOGSTAGING/$Filename", "$LOGPROCESSING/$Filename";
+		}
+		$RotateTime = mktime(localtime());
+		$Filename = "syslog.$RotateTime";
+		open(LOG, ">$LOGSTAGING/$Filename");
+		$RotateTime += 600;
+	}
 }
