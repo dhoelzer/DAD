551d5cba6ab846f0e1574efa1e5b30c8bad31afa
David Hoelzer <dhoelzer@Davids-Mac-Pro.local>
2014-11-16 12:31:14 -0500

    Confusing Perl and PHP

--- jobs/Alerts/IMAPAuthFailures.pl
+++ jobs/Alerts/IMAPAuthFailures.pl
@@ -16,9 +16,9 @@ while($arow = shift(@$result_ref))
 	{
 		@row = @$arow;
 		print "\t@row\n";
-		print "\t\t$row[3] - $row[4]\n";
-		$server = $row[11];
-		$user = $row[40];
+		@fields = split(/ /, $row[3]);
+		$server = $fields[11];
+		$user = $fields[40];
 		$failures{"$user on $server"} = 1;
 	}
 foreach(keys %failures)
