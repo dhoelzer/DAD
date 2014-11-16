9fb837f437264736c4ecece196ec142551e1840d
David Hoelzer <dhoelzer@Davids-Mac-Pro.local>
2014-11-16 12:05:44 -0500

    Confusing Perl and PHP

--- jobs/Alerts/IMAPAuthFailures.pl
+++ jobs/Alerts/IMAPAuthFailures.pl
@@ -8,7 +8,7 @@ require "../Reports/Reports.pm";
 $LastChecked = $ARGV[0];
 $Severity = 2;
 $AlertDescription = "IMAP Authentication Failures";
-$result_ref = &GetEventsByStringsPosition($LastChecked, "imap", 14, "auth", 16, "failure;", 20);
+$result_ref = &GetEventsByStringsPosition($LastChecked, "imap", 13, "auth", 15, "failure;", 19);
 $num_results = @$result_ref;
 print "results: $num_results\n";
 
