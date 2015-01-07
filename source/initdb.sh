3a1acdaf5eec588064c0d2ff313400ae70156f93
David Hoelzer <dhoelzer@auditcasts.com>
2015-01-07 16:23:28 -0500

    Switching to Rails model

--- source/initdb.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-echo Initializing schema...
-mysql -u root -p < Creates.sql
-echo Installing stored procedures
-mysql -u root -p < Procedures.sql
-echo Importing starter data...
-mysql -u root -p < Starter_Data.sql
