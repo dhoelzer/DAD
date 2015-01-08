acf94bcd31770abbc376699910c30feea60a219a
David Hoelzer <dhoelzer@auditcasts.com>
2015-01-08 08:22:11 -0500

    Added some stats to the syslog importer.

--- import_syslog.rb
+++ import_syslog.rb
@@ -1,12 +1,16 @@
 #!/usr/bin/env ruby
 require './config/environment.rb'
 
-Dir['Logs/LogsToProcess/*'].each do |log|
+pending_logs = Dir['Logs/LogsToProcess/*']
+processed = 0
+pending_logs.each do |log|
+  processed += 1
+  puts "Processing #{log}:  Log #{processed} out of #{pending_logs.count}"
   file = File.open(log)
   file.each_line do |line|
     Event.storeEvent(line)
   end
-  print "There were #{Word.number_of_cached_words} words cached and there are #{Word.count} words total.  #{Word.added} words were added.\n"
+  print "There are #{Word.number_of_cached_words} words currently cached and there are #{Word.count} words total.  #{Word.added} words were added.\n"
     # move log
 end
 Event.performPendingInserts
