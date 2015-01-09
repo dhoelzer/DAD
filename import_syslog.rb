a54aef0659c8860e042b0676e5537c7ada774daf
David Hoelzer <dhoelzer@auditcasts.com>
2015-01-08 21:53:44 -0500

    Adjusted to move processed logs.

--- import_syslog.rb
+++ import_syslog.rb
@@ -1,7 +1,8 @@
 #!/usr/bin/env ruby
 require './config/environment.rb'
 
-pending_logs = Dir['Logs/LogsToProcess/*'].sort
+Dir.chdir("Logs/LogsToProcess")
+pending_logs = Dir['*'].sort
 processed = 0
 pending_logs.each do |log|
   processed += 1
@@ -10,7 +11,8 @@ pending_logs.each do |log|
   file.each_line do |line|
     Event.storeEvent(line)
   end
+  file.close
   print ">>> There are #{Word.number_of_cached_words} words currently cached and there are #{Word.count} words total.  #{Word.added} words were added.\n"
-    # move log
+  File.rename log "../ProcessedLogs/#{log}"
 end
 Event.performPendingInserts
