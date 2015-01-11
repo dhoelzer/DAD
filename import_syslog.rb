436d304e5a25f921e5c68941ab629fcd6169c3ca
David Hoelzer <dhoelzer@auditcasts.com>
2015-01-10 20:28:59 -0500

    Handle control C

--- import_syslog.rb
+++ import_syslog.rb
@@ -4,21 +4,28 @@ require './config/environment.rb'
 Dir.chdir("Logs/LogsToProcess")
 pending_logs = Dir['*'].sort
 processed = 0
-pending_logs.each do |log|
-  processed += 1
-  puts "Processing #{log}:  Log #{processed} out of #{pending_logs.count}"
-  file = File.open(log)
-  file.each_line do |line|
-    begin
-      Event.storeEvent(line)
-    rescue
-      puts "Error processing #{log}!"
-      Event.performPendingInserts
-      exit
+begin
+  pending_logs.each do |log|
+    processed += 1
+    puts "Processing #{log}:  Log #{processed} out of #{pending_logs.count}"
+    file = File.open(log)
+
+    file.each_line do |line|
+      begin
+        Event.storeEvent(line)
+      rescue
+        puts "Error processing #{log}!"
+        puts e
+        Event.performPendingInserts
+        exit
+      end
     end
+    file.close
+    print ">>> There are #{Word.number_of_cached_words} words currently cached and there are #{Word.count} words total.  #{Word.added} words were added.\n"
+    File.rename(log, "../ProcessedLogs/#{log}")
   end
-  file.close
-  print ">>> There are #{Word.number_of_cached_words} words currently cached and there are #{Word.count} words total.  #{Word.added} words were added.\n"
-  File.rename(log, "../ProcessedLogs/#{log}")
+rescue Interrupt
+  puts "\nHalting operations... Emptying memory queues..."
+  Event.performPendingInserts
 end
 Event.performPendingInserts
