819fdf27045115b335a964145540c758e2e7cacd
David Hoelzer <dhoelzer@auditcasts.com>
2015-02-07 19:41:36 -0500

    Cleanup

--- app/models/event.rb
+++ app/models/event.rb
@@ -52,6 +52,7 @@ class Event < ActiveRecord::Base
     # Will add back in word stat logic and rebuild the massive sub selects even though it feels really wrong.
     #  select distinct e.event_id from (select distinct a.event_id from events_words as a where a.event_id in (select distinct b.event_id from events_words as b where b.word_id=8352832 and b.generated>NOW()-'1 day'::interval) and a.word_id=8338947) as e;
     events_that_match = connection.execute(sql)
+    puts events_that_match
     events_that_match.map { |e| event_ids << e["event_id"]}
     event_ids = event_ids[-100,100]
     @events = Event.order(generated: :asc).includes(:positions, :words).where("id in (?)", event_ids)
