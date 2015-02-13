5a0abdb2260b9e0244a08ec44da93777e0feae2d
David Hoelzer <dhoelzer@auditcasts.com>
2015-02-13 07:29:05 -0500

    Log processor now long-running with log purging

--- import_syslog.rb
+++ import_syslog.rb
@@ -1,30 +1,48 @@
 #!/usr/bin/env ruby
 require './config/environment.rb'
 
-Dir.chdir("Logs/LogsToProcess")
-pending_logs = Dir['*'].sort
-processed = 0
-begin
-  pending_logs.each do |log|
-    processed += 1
-    puts "Processing #{log}:  Log #{processed} out of #{pending_logs.count}"
-    file = File.open(log)
+def process_logs
+  pending_logs = Dir['*'].sort
+  processed = 0
+  begin
+    pending_logs.each do |log|
+      processed += 1
+      puts "Processing #{log}:  Log #{processed} out of #{pending_logs.count}"
+      file = File.open(log)
 
-    file.each_line do |line|
-      begin
-        Event.storeEvent(line)
-      rescue Exception => e
-        puts "Error processing #{log}!"
-        puts e
-        Event.performPendingInserts
-        exit
+      file.each_line do |line|
+        begin
+          Event.storeEvent(line)
+        rescue Exception => e
+          puts "Error processing #{log}!"
+          puts e
+          Event.performPendingInserts
+          exit
+        end
       end
+      file.close
+      File.rename(log, "../ProcessedLogs/#{log}")
     end
-    file.close
-    File.rename(log, "../ProcessedLogs/#{log}")
+  rescue Interrupt
+    puts "\nHalting operations... Emptying memory queues..."
+    Event.performPendingInserts
   end
-rescue Interrupt
-  puts "\nHalting operations... Emptying memory queues..."
   Event.performPendingInserts
 end
-Event.performPendingInserts
+
+def purge_logs
+  # Purge logs older than 1 day ago from now.
+  system("cd ../ProcessedLogs;find . -mtime +1 | xargs rm -Rf")
+end
+
+Dir.chdir("Logs/LogsToProcess")
+referenceTime = Time.now
+while 1 do
+  process_logs
+  if Time.now-12.hours > referenceTime then
+    puts "Purged logs #{Time.now}"
+    referenceTime = Time.now
+    purge_logs
+  end
+  sleep(1.minute)
+end
