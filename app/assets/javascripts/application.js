57028505c580045715369161afa791a0392efa2e
David Hoelzer <dhoelzer@auditcasts.com>
2015-02-13 08:24:30 -0500

    Adjust disk graph

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,7 +15,37 @@
 //= require turbolinks
 //= require highcharts-custom.js
 //= require_tree .
-	
+
+function draw_diskspace_graph(chart_div, title, graph_data)
+{
+   return new Highcharts.Chart({
+      chart: {
+        renderTo: chart_div,
+		animation: false,
+      },
+	 colors: {
+		['#ff1010', '#10ff10']
+	 },
+	 credits: {
+	    enabled: false
+	  },
+      title: {
+        text: title
+      },
+	  plotOptions: {
+		series: {
+			animation: false,
+		}
+	},
+      series: [
+        {
+          type: "pie",
+          name: "Disk Percentage:",
+          data: graph_data
+        }
+      ]
+    });
+}
  
 function draw_graph(chart_div, title, graph_data)
 {
