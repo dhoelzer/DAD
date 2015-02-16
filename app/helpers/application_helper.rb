202e74ca29dde6a25212c755391e252a1157856b
David Hoelzer <dhoelzer@auditcasts.com>
2015-02-16 07:56:41 -0500

    added timezone helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,17 @@ module ApplicationHelper
   def is_active(controller)       
     params[:controller] == controller ? "active" : "inactive"     
   end
+  
+  def tooltip_timestamp(timestamp=Time.now)
+    timezones = [
+      "Pacific Time (US & Canada)",
+      "Asia/Seoul",
+      "Europe/London",
+      "Asia/Shanghai"]
+    tooltip = ""
+    timezones.each do |zone|
+      tooltip = tooltip + "#{zone}: " + timestamp.in_time_zone(zone) + "<br>"
+    end
+    return tooltip
+  end
 end
