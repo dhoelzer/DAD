a988edb672d52e5a587b1c69314bb18beb09a4fd
David Hoelzer <dhoelzer@enclaveforensics.com>
2015-02-24 01:57:38 -0500

    Correct gauge representation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -165,18 +165,17 @@
 					distance: -20,
 					rotation: 'auto'
 				},
+				plotBands: [{
+					from: (average - (average * 0.2)),
+					to: (average + (average * 0.2)),
+					color: '#104310',
+					innerRadius: '100%',
+					outerRadius: '105%'
+				}],
 				tickLength: 5,
 				minorTickLength: 5,
 				endOnTick: false
 			},
-		    plotBands: [{
-		                   from: (average - (average * 0.2)),
-		                   to: (average + (average * 0.2)),
-		                   color: '#104310',
-		                   innerRadius: '100%',
-		                   outerRadius: '105%'
-		               }],
-				   ],
 			series: [
 				{
 					type: "gauge",
