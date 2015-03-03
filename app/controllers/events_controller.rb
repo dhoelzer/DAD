d43c5810219bffde4b1f298e1cd4b1f8f227833a
David Hoelzer <dhoelzer@auditcasts.com>
2015-03-03 10:24:07 -0500

    Use sent timeframe for search scope

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -18,7 +18,7 @@ class EventsController < ApplicationController
 
   def search
     start_time = Time.now
-    @events = Event.search(params[:search_terms], params[:timeframe].to_i)
+    @events = Event.search(params[:search_terms], Time.now - params[:timeframe].to_i)
     @previous_search = params[:search_terms]
     @search_time = Time.now - start_time
     respond_to do |format|
