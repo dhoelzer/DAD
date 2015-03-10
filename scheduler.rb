88e7e451b40e71928e3940900a17bbef8e30ae76
David Hoelzer <dhoelzer@auditcasts.com>
2015-03-10 08:30:30 -0400

    Add rescue handling to scheduled jobs

--- scheduler.rb
+++ scheduler.rb
@@ -5,7 +5,11 @@ while(true) do
   jobs = Job.where("next_run < ?", Time.now)
   jobs.each do |job|
     puts "Executing #{job.description}"
-    eval(job.script)
+    begin
+      eval(job.script)
+    rescue Exception => e
+      Alert.genericAlert(description: "Scheduled job failed: #{job.name} -> #{e.backtrace}", short_description: "Scheduled job failed: #{job.name}", criticality: 5)
+    end
     job.next_run = Time.now + (job.frequency).seconds
     job.last_run = Time.now
     job.save
