e8cb8a2a1ec1b7aa80ec5bc743c70bbacdff7c62
David Hoelzer <dhoelzer@auditcasts.com>
2015-03-23 17:32:48 -0400

    Comments have users.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,6 @@
 class Comment < ActiveRecord::Base
   belongs_to :alert
+  belongs_to :user
   
   def self.hidden?(current_user = nil)
     return true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   has_and_belongs_to_many :roles
+  has_many :comments
   has_many :rights, through: :roles
   has_one :session
   
