5e26c93d795e2288b051ea108a53c4cac49075ce
David Hoelzer <dhoelzer@auditcasts.com>
2015-03-24 05:52:06 -0400

    Set initial value for attempts.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -132,7 +132,7 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(user_params)
-
+    @user.attempts = 0
     respond_to do |format|
       if @user.save
         format.html { redirect_to @user, notice: 'User was successfully created.' }
