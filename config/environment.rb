31085be62557e4a5be278c4dff7830e4957f5bed
David Hoelzer <dhoelzer@auditcasts.com>
2015-04-21 09:31:16 -0400

    Switch to myisam and maria

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,16 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+# Override innoDB default
+module ActiveRecord
+        module ConnectionAdapters
+                class Mysql2Adapter < AbstractAdapter
+                        def create_table(table_name, options = {}) #:nodoc:
+                                super(table_name, options.reverse_merge(:options => "ENGINE=MyISAM"))
+                        end
+                end
+        end
+end
+
 # Initialize the Rails application.
 Events::Application.initialize!
