ab177cef8b1558311cd2040eb9381d13526d23c1
David Hoelzer <dhoelzer@auditcasts.com>
2015-04-21 09:40:10 -0400

    Override mysql2

--- config/environment.rb
+++ config/environment.rb
@@ -2,14 +2,15 @@
 require File.expand_path('../application', __FILE__)
 
 # Override innoDB default
+require 'active_record/connection_adapters/mysql2_adapter'
 module ActiveRecord
-        module ConnectionAdapters
-                class Mysql2Adapter < AbstractAdapter
-                        def create_table(table_name, options = {}) #:nodoc:
-                                super(table_name, options.reverse_merge(:options => "ENGINE=MyISAM"))
-                        end
-                end
-        end
+  module ConnectionAdapters
+    class Mysql2Adapter
+      def create_table(table_name, options = {}) #:nodoc:
+        super(table_name, options.reverse_merge(:options => "ENGINE=MyISAM"))
+      end
+    end
+  end
 end
 
 # Initialize the Rails application.
