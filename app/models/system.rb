b0e0d41f434e8e4c9aea407ebbe223211b743966
David Hoelzer <dhoelzer@auditcasts.com>
2015-04-23 11:51:54 -0400

    24 hour pull was broken.

--- app/models/system.rb
+++ app/models/system.rb
@@ -13,7 +13,7 @@ class System < ActiveRecord::Base
   
   def self.reportingInLastDays(days)
     connection = ActiveRecord::Base.connection
-    events_since = Time.now - (days * 86400)
+    events_since = (Time.now - (days * 86400)).to_s(:db)
     sql = "select system_id,b.name,count(*) from events as a join systems as b on  a.system_id=b.id where generated>'#{events_since}' group by system_id,b.name order by b.name asc"
     connection.execute(sql)    
   end
