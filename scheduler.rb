cba4323ce52b7eee95320bdb055c887f2175a650
David Hoelzer <dhoelzer@auditcasts.com>
2015-05-28 07:08:09 -0400

    Changed scheduler to only display errors in the console.

--- scheduler.rb
+++ scheduler.rb
@@ -1,20 +1,19 @@
 #!/usr/bin/env ruby
 require './config/environment.rb'
+puts "The scheduler is now running.  Only errors will be displayed here."
 puts $SAFE
 while(true) do
   jobs = Job.where("next_run < ?", Time.now)
   jobs.each do |job|
-    puts "Executing #{job.description}"
     begin
       eval(job.script)
     rescue Exception => e
       Alert.genericAlert(description: "Scheduled job failed: #{job.name} -> #{e.backtrace}", short_description: "Scheduled job failed: #{job.name}", criticality: 5)
+      puts "Error executing #{job.description}"
     end
     job.next_run = Time.now + (job.frequency).seconds
     job.last_run = Time.now
     job.save
-    puts "Job rescheduled for #{job.next_run}"
   end
-  puts "Sleeping"
   sleep(60)
 end
