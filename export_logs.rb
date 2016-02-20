a83aa129709046ac8d9db145830aa9db08ae8954
David Hoelzer <dhoelzer@auditcasts.com>
2016-02-20 08:13:37 -0500

    Find in batches

--- export_logs.rb
+++ export_logs.rb
@@ -1,4 +1,9 @@
 #!/usr/bin/env ruby
 require './config/environment.rb'
 
-Event.all.each { |event| puts event.reconstitute }
+count = 0
+Event.find_each do |event| 
+  count = count + 1
+  puts event.reconstitute
+  puts "#{ActiveSupport::NumberHelper.number_to_delimited(count)} exported..." if count % 100000 == 0
+end
