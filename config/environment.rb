896c1c7b0fe3e25d197f2fe4bd3c947c51f6532e
David Hoelzer <dhoelzer@auditcasts.com>
2016-02-20 13:33:52 -0500

    Remove mysql2 adapter

--- config/environment.rb
+++ config/environment.rb
@@ -1,21 +1,6 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
-if Rails.env == "production" then
-  # Override innoDB default
-  require 'active_record/connection_adapters/mysql2_adapter'
-  module ActiveRecord
-    module ConnectionAdapters
-      class Mysql2Adapter
-        def create_table(table_name, options = {}) #:nodoc:
-          super(table_name, options.reverse_merge(:options => "ENGINE=MyISAM"))
-        end
-      end
-    end
-  end
-
-  ActiveRecord::ConnectionAdapters::Mysql2Adapter::NATIVE_DATABASE_TYPES[:primary_key] = "BIGINT UNSIGNED DEFAULT NULL auto_increment PRIMARY KEY"
-end
 
 # Initialize the Rails application.
 Events::Application.initialize!
