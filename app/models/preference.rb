923f085817b457f0975d7ca33c8a08edea6d36be
David Hoelzer <dhoelzer@auditcasts.com>
2016-09-22 12:45:49 -0400

    Enable viewing

--- app/models/preference.rb
+++ app/models/preference.rb
@@ -1,2 +1,8 @@
 class Preference < ActiveRecord::Base
+  def self.hidden?(current_user = nil)
+    return true if current_user.nil?
+    return true unless current_user.has_right?("Viewer")
+    return false
+  end
+
 end
